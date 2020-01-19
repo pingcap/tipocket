0be84be40de5e747c1e11045a99a55e9105068d0
zhouqiang <zhouqiang.cl@gmail.com>
2020-01-19 16:24:28 +0800

    add license (#16)

--- go-sqlsmith/builtin/genetate.go
+++ go-sqlsmith/builtin/genetate.go
@@ -1,3 +1,16 @@
+// Copyright 2019 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // generate is for future usage
 // which aim to make a builtin function combination or chain by a given type
 // the returned builtin function combination can be multiple function call
