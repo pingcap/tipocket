87843eb9185980658d588c16dd18bee41ddbed25
mahjonp <junpeng.man@gmail.com>
2020-02-13 18:44:11 +0800

    add partition_one
    Signed-off-by: mahjonp <junpeng.man@gmail.com>

---
+++ pkg/nemesis/kill.go
@@ -0,0 +1,107 @@
+package nemesis
+
+import (
+	"context"
+	"log"
+	"math/rand"
+	"strings"
+	"time"
+
+	chaosv1alpha1 "github.com/pingcap/chaos-mesh/api/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/pingcap/chaos-mesh/api/v1alpha1"
+	"github.com/pingcap/tipocket/pkg/cluster"
+	"github.com/pingcap/tipocket/pkg/core"
+)
+
+type killGenerator struct {
+	name string
+}
+
+func (g killGenerator) Generate(nodes []cluster.Node) []*core.NemesisOperation {
+	n := 1
+
+	// This part decide how many machines to apply pod-failure
+	switch g.name {
+	case "minor_kill":
+		n = len(nodes)/2 - 1
+	case "major_kill":
+		n = len(nodes)/2 + 1
+	case "all_kill":
+		n = len(nodes)
+	default:
+		n = 1
+	}
+
+	return killNodes(nodes, n)
+}
+
+func (g killGenerator) Name() string {
+	return string(core.PodFailure)
+}
+
+func killNodes(nodes []cluster.Node, n int) []*core.NemesisOperation {
+	ops := make([]*core.NemesisOperation, len(nodes))
+
+	// randomly shuffle the indices and get the first n nodes to be partitioned.
+	indices := shuffleIndices(len(nodes))
+
+	for i := 0; i < n; i++ {
+		ops[indices[i]] = &core.NemesisOperation{
+			Type: core.PodFailure,
+			// Note: Maybe I should just store cluster info here.
+			InvokeArgs:  nil,
+			RecoverArgs: nil,
+			RunTime:     time.Second * time.Duration(rand.Intn(120)+60),
+		}
+	}
+
+	return ops
+}
+
+// NewKillGenerator creates a generator.
+// Name is random_kill, minor_kill, major_kill, and all_kill.
+func NewKillGenerator(name string) core.NemesisGenerator {
+	return killGenerator{name: name}
+}
+
+type kill struct {
+	k8sNemesisClient
+}
+
+func (k kill) Invoke(ctx context.Context, node cluster.Node, args ...interface{}) error {
+	log.Printf("Creating pod-kill with node %s(ns:%s)\n", node.PodName, node.Namespace)
+	podChaos := podTag(node.Namespace, node.Namespace, node.PodName, v1alpha1.PodFailureAction)
+	return k.cli.ApplyPodChaos(ctx, &podChaos)
+}
+
+func (k kill) Recover(ctx context.Context, node cluster.Node, args ...interface{}) error {
+	log.Printf("Recover pod-kill with node %s(ns:%s)\n", node.PodName, node.Namespace)
+	podChaos := podTag(node.Namespace, node.Namespace, node.PodName, v1alpha1.PodFailureAction)
+	return k.cli.CancelPodChaos(ctx, &podChaos)
+}
+
+func (kill) Name() string {
+	return string(core.PodFailure)
+}
+
+func podTag(ns string, chaosNs string, name string, chaos chaosv1alpha1.PodChaosAction) chaosv1alpha1.PodChaos {
+	pods := make(map[string][]string)
+	pods[ns] = []string{name}
+
+	return chaosv1alpha1.PodChaos{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: strings.Join([]string{name, string(chaos)}, "-"),
+			// TODO: this might be a chaos ns, so let's take it carefully.
+			Namespace: chaosNs,
+		},
+		Spec: chaosv1alpha1.PodChaosSpec{
+			Selector: chaosv1alpha1.SelectorSpec{
+				Pods: pods,
+			},
+			Action: chaos,
+			Mode:   chaosv1alpha1.OnePodMode,
+		},
+	}
+}
---
+++ pkg/nemesis/network_partition.go
@@ -0,0 +1,160 @@
+package nemesis
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	chaosv1alpha1 "github.com/pingcap/chaos-mesh/api/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/pingcap/tipocket/pkg/cluster"
+	"github.com/pingcap/tipocket/pkg/core"
+)
+
+type networkPartitionGenerator struct {
+	name string
+}
+
+func (g networkPartitionGenerator) Generate(nodes []cluster.Node) []*core.NemesisOperation {
+	n := 1
+	switch g.name {
+	case "partition_one":
+		n = 1
+	default:
+		n = 1
+	}
+	return partitionNodes(nodes, n)
+}
+
+func (g networkPartitionGenerator) Name() string {
+	return g.name
+}
+
+func partitionNodes(nodes []cluster.Node, n int) []*core.NemesisOperation {
+	ops := make([]*core.NemesisOperation, len(nodes))
+
+	// randomly shuffle the indices and get the first n nodes to be partitioned.
+	indices := shuffleIndices(len(nodes))
+
+	var onePartNodes []cluster.Node
+	var anotherPartNodes []cluster.Node
+
+	for i := 0; i < n; i++ {
+		onePartNodes = append(onePartNodes, nodes[indices[i]])
+	}
+	for i := n; i < len(nodes); i++ {
+		anotherPartNodes = append(anotherPartNodes, nodes[indices[i]])
+	}
+
+	ops[0] = &core.NemesisOperation{
+		Type:        core.NetworkPartition,
+		InvokeArgs:  []interface{}{onePartNodes, anotherPartNodes},
+		RecoverArgs: []interface{}{onePartNodes, anotherPartNodes},
+		RunTime:     time.Second * time.Duration(rand.Intn(120)+60),
+	}
+
+	return ops
+}
+
+// NewNetworkPartitionGenerator creates a generator.
+// Name is partition-one, etc.
+func NewNetworkPartitionGenerator(name string) core.NemesisGenerator {
+	return networkPartitionGenerator{name: name}
+}
+
+type networkPartition struct {
+	k8sNemesisClient
+}
+
+func (n networkPartition) Invoke(ctx context.Context, _ cluster.Node, args ...interface{}) error {
+	onePart, anotherPart := extractTwoParts(args...)
+	return n.cli.ApplyNetChaos(&chaosv1alpha1.NetworkChaos{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-%s", onePart[0].Namespace, chaosv1alpha1.PartitionAction),
+			Namespace: onePart[0].Namespace,
+		},
+		Spec: chaosv1alpha1.NetworkChaosSpec{
+			Action: chaosv1alpha1.PartitionAction,
+			Mode:   chaosv1alpha1.AllPodMode,
+			Selector: chaosv1alpha1.SelectorSpec{
+				Pods: map[string][]string{
+					onePart[0].Namespace: extractPodNames(onePart),
+				},
+			},
+			Direction: chaosv1alpha1.Both,
+			Target: chaosv1alpha1.PartitionTarget{
+				TargetSelector: chaosv1alpha1.SelectorSpec{
+					Pods: map[string][]string{
+						anotherPart[0].Namespace: extractPodNames(anotherPart),
+					},
+				},
+				TargetMode: chaosv1alpha1.AllPodMode,
+			},
+		},
+	})
+}
+
+func (n networkPartition) Recover(ctx context.Context, _ cluster.Node, args ...interface{}) error {
+	onePart, anotherPart := extractTwoParts(args...)
+	return n.cli.ApplyNetChaos(&chaosv1alpha1.NetworkChaos{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-%s", onePart[0].Namespace, chaosv1alpha1.PartitionAction),
+			Namespace: onePart[0].Namespace,
+		},
+		Spec: chaosv1alpha1.NetworkChaosSpec{
+			Action: chaosv1alpha1.PartitionAction,
+			Mode:   chaosv1alpha1.AllPodMode,
+			Selector: chaosv1alpha1.SelectorSpec{
+				Pods: map[string][]string{
+					onePart[0].Namespace: extractPodNames(onePart),
+				},
+			},
+			Direction: chaosv1alpha1.Both,
+			Target: chaosv1alpha1.PartitionTarget{
+				TargetSelector: chaosv1alpha1.SelectorSpec{
+					Pods: map[string][]string{
+						anotherPart[0].Namespace: extractPodNames(anotherPart),
+					},
+				},
+				TargetMode: chaosv1alpha1.AllPodMode,
+			},
+		},
+	})
+}
+
+func (n networkPartition) Name() string {
+	return string(core.NetworkPartition)
+}
+
+func extractTwoParts(args ...interface{}) ([]cluster.Node, []cluster.Node) {
+	var networkParts [][]cluster.Node
+	var onePart []cluster.Node
+	var anotherPart []cluster.Node
+
+	for _, arg := range args {
+		networkPart := arg.([]cluster.Node)
+		networkParts = append(networkParts, networkPart)
+	}
+
+	if len(networkParts) != 2 {
+		log.Panicf("expect two network parts, got %+v", networkParts)
+	}
+	onePart = networkParts[0]
+	anotherPart = networkParts[1]
+	if len(onePart) < 1 || len(anotherPart) < 1 {
+		log.Panicf("expect non-empty two parts, got %+v and %+v", onePart, anotherPart)
+	}
+	return onePart, anotherPart
+}
+
+func extractPodNames(nodes []cluster.Node) []string {
+	var podNames []string
+
+	for _, node := range nodes {
+		podNames = append(podNames, node.PodName)
+	}
+	return podNames
+}
