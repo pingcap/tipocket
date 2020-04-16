6039ff3057ce28287a75208ab52c386661d35ebe
mapleFU <1506118561@qq.com>
2020-04-16 13:43:19 +0800

    [ADD] Add contents for cm.
    Signed-off-by: mapleFU <1506118561@qq.com>

--- pkg/elle/txn/cycle.go
+++ pkg/elle/txn/cycle.go
@@ -20,6 +20,7 @@ type CycleAnomalySpecType struct {
 }
 
 var CycleAnomalySpecs map[string]CycleAnomalySpecType
+var CycleTypeNames map[string]struct{}
 
 func fromRels(rels ...core.Rel) CycleAnomalySpecType {
 	return fromRelsWithFilter(nil, rels...)
@@ -77,4 +78,59 @@ func init()  {
 		"G-single-realtime": fromFirstRelAndRestWithFilter(filterClosure("G-single-realtime"), core.RW, core.WW, core.WR, core.Realtime),
 		"G2-item-realtime": fromFirstRelAndRestWithFilter(filterClosure("G2-item-realtime"), core.RW, core.WW, core.WR, core.Realtime, core.RW),
 	}
+
+	CycleTypeNames = map[string]struct{}{
+		"G-nonadjacent-process": {},
+		"G-nonadjacent-realtime": {},
+	}
+
+	for k, _ := range CycleAnomalySpecs {
+		CycleTypeNames[k] = struct{}{}
+	}
+}
+
+// CycleExplainWrapper is a ICycleExplainer, it's also a wrapper for core.
+type CycleExplainerWrapper struct {}
+
+func (c CycleExplainerWrapper) ExplainCycle(pairExplainer core.DataExplainer, circle core.Circle) (core.Circle, []core.Step) {
+	panic("implement me")
+}
+
+func (c CycleExplainerWrapper) RenderCycleExplanation(explainer core.DataExplainer, circle core.Circle) string {
+	return CycleExplainer{}.RenderCycleExplanation(explainer, circle)
+}
+
+// NonadjacentRW is an strange helper function. It returns (valid, rw-count, current-is-rw).
+// when initialize, please provide [0, false].
+// This fn ensures that no :rw is next to another by testing successive edge
+//  types. In addition, we ensure that the first edge in the cycle is not an rw.
+//  Cycles must have at least two edges, and in order for no two rw edges to be
+//  adjacent, there must be at least one non-rw edge among them. This constraint
+//  ensures a sort of boundary condition for the first and last nodes--even if
+//  the last edge is rw, we don't have to worry about violating the nonadjacency
+//  property when we jump to the first.
+func NonadjacentRW(n int, lastIsRw bool, rel core.Rel) (bool, int, bool) {
+	if rel != core.RW {
+		return true, n, lastIsRw
+	} else {
+		return lastIsRw, n + 1, true
+	}
+}
+
+type AdditionalGraphOption struct {
+	ConsistencyModels []core.ConsistencyModelName
+	Anomalies []string
+
+}
+
+func AdditionalGraphs(opts interface{})  {
+	panic("implement me")
+}
+
+func prohibitAnomalyTypes(cm []core.ConsistencyModelName, anomalies []string) map[core.Anomaly]struct{} {
+	panic("implement me")
+}
+
+func compactAnomalies(anomalies ...core.Anomaly) map[core.Anomaly]struct{} {
+	panic("implement me")
 }
