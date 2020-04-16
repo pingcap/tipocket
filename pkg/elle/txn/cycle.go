475dbe6f9354173d4a83ec0e35ae2d98ee56fdc8
mapleFU <1506118561@qq.com>
2020-04-16 16:06:42 +0800

    [ADD] Add code for helper_fn for cycle.
    Signed-off-by: mapleFU <1506118561@qq.com>

--- pkg/elle/txn/cycle.go
+++ pkg/elle/txn/cycle.go
@@ -1,6 +1,10 @@
 package txn
 
-import "github.com/pingcap/tipocket/pkg/elle/core"
+import (
+	"strings"
+
+	"github.com/pingcap/tipocket/pkg/elle/core"
+)
 
 type FilterExType = func(cycleCase CycleCase) bool
 
@@ -16,11 +20,17 @@ type CycleAnomalySpecType struct {
 	// A predicate over a cycle explanation. We use this to restrict cycles to e.g. *just* G2 instead of G-single.
 	FilterEx FilterExType
 
-	With      interface{} // seems only G-nonadjacent is used, it's a little complicated
+	With interface{} // seems only G-nonadjacent is used, it's a little complicated
 }
 
 var CycleAnomalySpecs map[string]CycleAnomalySpecType
 var CycleTypeNames map[string]struct{}
+var UnknownAnomalyTypes map[string]struct{}
+
+// Anomaly types involving realtime edges.
+var RealtimeAnalysisTypes map[string]struct{}
+
+var ProcessAnalysisTypes map[string]struct{}
 
 func fromRels(rels ...core.Rel) CycleAnomalySpecType {
 	return fromRelsWithFilter(nil, rels...)
@@ -48,7 +58,7 @@ func fromFirstRelAndRestWithFilter(filter FilterExType, first core.Rel, rests ..
 	}
 	return CycleAnomalySpecType{
 		FirstRel: first,
-		RestRels:     restSet,
+		RestRels: restSet,
 		FilterEx: filter,
 	}
 }
@@ -58,39 +68,55 @@ func filterClosure(required string) FilterExType {
 	return nil
 }
 
-func init()  {
+func init() {
 	CycleAnomalySpecs = map[string]CycleAnomalySpecType{
-		"G0": fromRelsWithFilter(core.WW),
-		"G1c": fromFirstRelAndRest(core.WR, core.WW, core.WR),
+		"G0":       fromRelsWithFilter(core.WW),
+		"G1c":      fromFirstRelAndRest(core.WR, core.WW, core.WR),
 		"G-single": fromFirstRelAndRest(core.RW, core.WW, core.WR),
 		// TODO: G-nonadjacent is not implemented
 
-		"G2-item": fromFirstRelAndRestWithFilter(filterClosure("G2-item"), core.RW, core.WR, core.RW, core.WW),
-		"G0-process": fromRelsWithFilter(filterClosure("G0-process"), core.WW, core.Process),
-		"G1c-process": fromFirstRelAndRestWithFilter(filterClosure("G1c-process"), core.RW, core.WW, core.WR, core.Process),
+		"G2-item":          fromFirstRelAndRestWithFilter(filterClosure("G2-item"), core.RW, core.WR, core.RW, core.WW),
+		"G0-process":       fromRelsWithFilter(filterClosure("G0-process"), core.WW, core.Process),
+		"G1c-process":      fromFirstRelAndRestWithFilter(filterClosure("G1c-process"), core.RW, core.WW, core.WR, core.Process),
 		"G-single-process": fromFirstRelAndRestWithFilter(filterClosure("G-single-process"), core.RW, core.WW, core.WR, core.Process),
-		"G2-item-process": fromFirstRelAndRestWithFilter(filterClosure("G2-item-process"), core.RW, core.WW, core.WR, core.RW, core.Process),
+		"G2-item-process":  fromFirstRelAndRestWithFilter(filterClosure("G2-item-process"), core.RW, core.WW, core.WR, core.RW, core.Process),
 
 		// realtime
 
-		"G0-realtime": fromRelsWithFilter(filterClosure("G0-realtime"), core.WW, core.Realtime),
-		"G1c-realtime": fromFirstRelAndRestWithFilter(filterClosure("G1c-realtime"), core.WR, core.WW, core.WR, core.Realtime),
+		"G0-realtime":       fromRelsWithFilter(filterClosure("G0-realtime"), core.WW, core.Realtime),
+		"G1c-realtime":      fromFirstRelAndRestWithFilter(filterClosure("G1c-realtime"), core.WR, core.WW, core.WR, core.Realtime),
 		"G-single-realtime": fromFirstRelAndRestWithFilter(filterClosure("G-single-realtime"), core.RW, core.WW, core.WR, core.Realtime),
-		"G2-item-realtime": fromFirstRelAndRestWithFilter(filterClosure("G2-item-realtime"), core.RW, core.WW, core.WR, core.Realtime, core.RW),
+		"G2-item-realtime":  fromFirstRelAndRestWithFilter(filterClosure("G2-item-realtime"), core.RW, core.WW, core.WR, core.Realtime, core.RW),
 	}
 
 	CycleTypeNames = map[string]struct{}{
-		"G-nonadjacent-process": {},
+		"G-nonadjacent-process":  {},
 		"G-nonadjacent-realtime": {},
 	}
 
 	for k, _ := range CycleAnomalySpecs {
 		CycleTypeNames[k] = struct{}{}
 	}
+
+	UnknownAnomalyTypes = map[string]struct{}{
+		"empty-transaction-graph": {},
+		"cycle-search-timeout":    {},
+	}
+
+	ProcessAnalysisTypes = map[string]struct{}{}
+	RealtimeAnalysisTypes = map[string]struct{}{}
+	for k, _ := range CycleAnomalySpecs {
+		if strings.Contains(k, "process") {
+			ProcessAnalysisTypes[k] = struct{}{}
+		}
+		if strings.Contains(k, "realtime") {
+			RealtimeAnalysisTypes[k] = struct{}{}
+		}
+	}
 }
 
 // CycleExplainWrapper is a ICycleExplainer, it's also a wrapper for core.
-type CycleExplainerWrapper struct {}
+type CycleExplainerWrapper struct{}
 
 func (c CycleExplainerWrapper) ExplainCycle(pairExplainer core.DataExplainer, circle core.Circle) (core.Circle, []core.Step) {
 	panic("implement me")
@@ -119,18 +145,60 @@ func NonadjacentRW(n int, lastIsRw bool, rel core.Rel) (bool, int, bool) {
 
 type AdditionalGraphOption struct {
 	ConsistencyModels []core.ConsistencyModelName
-	Anomalies []string
-
+	Anomalies         []string
 }
 
-func AdditionalGraphs(opts interface{})  {
-	panic("implement me")
+type GraphFunc = func(core.History) (core.Anomalies, core.DirectedGraph, core.DataExplainer)
+
+// AdditionalGraphs determines what additional graphs we'll need to consider for this analysis.
+func AdditionalGraphs(opts AdditionalGraphOption, additionalGraphs []GraphFunc) []GraphFunc {
+	ats := reportableAnomalyTypes(opts.ConsistencyModels, opts.Anomalies)
+	var graphFn GraphFunc
+	if hasIntersection(ats, RealtimeAnalysisTypes) {
+		graphFn = core.RealtimeGraph
+	} else if hasIntersection(ats, ProcessAnalysisTypes) {
+		graphFn = core.ProcessGraph
+	} else {
+		return additionalGraphs
+	}
+	return append(additionalGraphs, graphFn)
 }
 
-func prohibitAnomalyTypes(cm []core.ConsistencyModelName, anomalies []string) map[core.Anomaly]struct{} {
-	panic("implement me")
+// Anomalies worth reporting on, even if they don't cause the test to fail.
+func reportableAnomalyTypes(cm []core.ConsistencyModelName, anomalies []string) map[string]struct{} {
+	dest := prohibitAnomalyTypes(cm, anomalies)
+	union(dest, UnknownAnomalyTypes)
+	return dest
 }
 
-func compactAnomalies(anomalies ...core.Anomaly) map[core.Anomaly]struct{} {
-	panic("implement me")
+func prohibitAnomalyTypes(cm []core.ConsistencyModelName, anomalies []string) map[string]struct{} {
+	a1 := core.AllAnomaliesImplying(cm)
+	a2 := core.AnomaliesProhibitedBy(anomalies)
+	dest := compactAnomalies(a1)
+	union(dest, compactAnomalies(a2))
+	return dest
+}
+
+func compactAnomalies(anomalies ...core.Anomaly) map[string]struct{} {
+	ret := map[string]struct{}{}
+	for _, v := range anomalies {
+		ret[v] = struct{}{}
+	}
+	return ret
+}
+
+func union(dest map[string]struct{}, unionSrc map[string]struct{}) {
+	for k, _ := range unionSrc {
+		dest[k] = struct{}{}
+	}
+}
+
+func hasIntersection(m1, m2 map[string]struct{}) bool {
+	for k, _ := range m2 {
+		_, e := m1[k]
+		if e {
+			return true
+		}
+	}
+	return false
 }
