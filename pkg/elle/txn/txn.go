7cb0823b0e1481e951723200c71c9f241093a135
mapleFU <1506118561@qq.com>
2020-04-16 16:36:51 +0800

    [Update] Update framework for scc.
    Signed-off-by: mapleFU <1506118561@qq.com>

--- pkg/elle/txn/txn.go
+++ pkg/elle/txn/txn.go
@@ -18,18 +18,13 @@ type CheckResult struct {
 //  :consistency-models, a set of additional :anomalies, an analyzer function,
 //  and a history. Analyzes the history and yields the analysis, plus an anomaly
 //  map like {:G1c [...]}.
-func Cycles(opts Opts, analyzer core.Analyzer, history core.History) core.Anomalies {
-	panic("implement me")
-}
-
-type CycleExplainer struct{}
-
-func (c *CycleExplainer) ExplainCycle(explainer core.DataExplainer, circle core.Circle) (core.Circle, []core.Step) {
-	panic("impl me")
-}
-
-func (c *CycleExplainer) RenderCycleExplanation(explainer core.DataExplainer, circle core.Circle) string {
-	panic("impl me")
+func cycles(analyzer core.Analyzer, history core.History) core.Anomalies {
+	checkedResult := core.Check(analyzer, history)
+	cases := CycleCases(checkedResult.Graph, checkedResult.Explainer, checkedResult.Sccs)
+	for k, v := range cases {
+		checkedResult.Anomalies[k] = v
+	}
+	return checkedResult.Anomalies
 }
 
 type CycleCase struct {
@@ -38,20 +33,32 @@ type CycleCase struct {
 	Type   string
 }
 
-func CycleCases(graph core.DirectedGraph, pairExplainer core.DataExplainer, sccs []core.SCC) map[string]CycleCase {
-	panic("impl me")
+func CycleCases(graph core.DirectedGraph, pairExplainer core.DataExplainer, sccs []core.SCC) map[string][]CycleCase {
+	g := FilteredGraphs(graph)
+	cases := map[string][]CycleCase{}
+	for _, scc := range sccs {
+		for _, v := range CycleCasesInScc(graph, g, pairExplainer, scc) {
+			if _, e := cases[v.Type]; !e {
+				cases[v.Type] = make([]CycleCase, 0)
+			}
+			cases[v.Type] = append(cases[v.Type], v)
+		}
+	}
+
+	return cases
 }
 
 // FilteredGraphs receives a graph and a collection of relations, return a new Graph filtered to just those relationships
-func FilteredGraphs(graph core.DirectedGraph, rels []interface{}) core.DirectedGraph {
+func FilteredGraphs(graph core.DirectedGraph) core.DirectedGraph {
 	panic("impl me")
 }
 
 // CycleCasesInScc searches a single SCC for cycle anomalies
-func CycleCasesInScc(graph core.DirectedGraph, explainer core.DataExplainer, scc core.SCC) []CycleCase {
+func CycleCasesInScc(graph core.DirectedGraph, filterGraph core.DirectedGraph, explainer core.DataExplainer, scc core.SCC) []CycleCase {
 	panic("impl me")
 }
 
-func init() {
-	//CycleAnomalySpecs =
+// CyclesWithDraw means "cycles!" in clojure.
+func CyclesWithDraw() {
+	panic("implement me")
 }
