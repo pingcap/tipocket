a481ba2e27ce1d2328ba442f30a7fb9c31e4d246
mwish <1506118561@qq.com>
2020-04-17 22:41:22 +0800

    Add code and test for txn (#190)
    Signed-off-by: mapleFU <1506118561@qq.com>

--- pkg/elle/txn/txn.go
+++ pkg/elle/txn/txn.go
@@ -1,6 +1,8 @@
 package txn
 
-import "github.com/pingcap/tipocket/pkg/elle/core"
+import (
+	"github.com/pingcap/tipocket/pkg/elle/core"
+)
 
 type Opts struct {
 }
@@ -12,32 +14,17 @@ type CheckResult struct {
 	ImpossibleModels []interface{}
 }
 
-type CycleAnomalySpec struct {
-	Rels      []core.Rel
-	FirstRels []core.Rel
-	RestRels  []core.Rel
-	With      interface{} // seems only G-nonadjacent is used, it's a little complicated
-	FilterEx  func(cycleCase CycleCase) bool
-}
-
-var CycleAnomalySpecs []CycleAnomalySpec
-
 // Takes an options map, including a collection of expected consistency models
 //  :consistency-models, a set of additional :anomalies, an analyzer function,
 //  and a history. Analyzes the history and yields the analysis, plus an anomaly
 //  map like {:G1c [...]}.
-func Cycles(opts Opts, analyzer core.Analyzer, history core.History) core.Anomalies {
-	panic("implement me")
-}
-
-type CycleExplainer struct{}
-
-func (c *CycleExplainer) ExplainCycle(explainer core.DataExplainer, circle core.Circle) (core.Circle, []core.Step) {
-	panic("impl me")
-}
-
-func (c *CycleExplainer) RenderCycleExplanation(explainer core.DataExplainer, circle core.Circle) string {
-	panic("impl me")
+func Cycles(analyzer core.Analyzer, history core.History) core.CheckResult {
+	checkedResult := core.Check(analyzer, history)
+	cases := CycleCases(checkedResult.Graph, checkedResult.Explainer, checkedResult.Sccs)
+	for k, v := range cases {
+		checkedResult.Anomalies[k] = v
+	}
+	return checkedResult
 }
 
 type CycleCase struct {
@@ -46,20 +33,64 @@ type CycleCase struct {
 	Type   string
 }
 
-func CycleCases(graph core.DirectedGraph, pairExplainer core.DataExplainer, sccs []core.SCC) map[string]CycleCase {
-	panic("impl me")
+func CycleCases(graph core.DirectedGraph, pairExplainer core.DataExplainer, sccs []core.SCC) map[string][]CycleCase {
+	g := FilteredGraphs(graph)
+	cases := map[string][]CycleCase{}
+	for _, scc := range sccs {
+		for _, v := range CycleCasesInScc(graph, g, pairExplainer, scc) {
+			if _, e := cases[v.Type]; !e {
+				cases[v.Type] = make([]CycleCase, 0)
+			}
+			cases[v.Type] = append(cases[v.Type], v)
+		}
+	}
+
+	return cases
 }
 
-// FilteredGraphs receives a graph and a collection of relations, return a new Graph filtered to just those relationships
-func FilteredGraphs(graph core.DirectedGraph, rels []interface{}) core.DirectedGraph {
-	panic("impl me")
+type FilterGraphFn = func(rels []core.Rel) *core.DirectedGraph
+
+// CycleCasesInScc searches a single SCC for cycle anomalies.
+// TODO: add timeout logic.
+func CycleCasesInScc(graph core.DirectedGraph, filterGraph FilterGraphFn, explainer core.DataExplainer, scc core.SCC) []CycleCase {
+	var cases []CycleCase
+	for _, v := range CycleAnomalySpecs {
+		var runtimeGraph *core.DirectedGraph
+		if v.Rels != nil {
+			runtimeGraph = filterGraph(setKeys(v.Rels))
+		} else {
+			runtimeGraph = &graph
+		}
+		var cycle *core.Circle
+		cycle = nil
+		if v.With != nil {
+			c := core.FindCycleWith(v.With, v.FilterPathState, *runtimeGraph, scc)
+			cycle = &c
+		} else if v.Rels != nil {
+			c := core.FindCycle(*runtimeGraph, scc)
+			cycle = &c
+		} else {
+			// Note: this requires find-cycle-starting-with
+			s1 := filterGraph([]core.Rel{v.FirstRel})
+			s2 := filterGraph(setKeys(v.RestRels))
+			c := core.FindCycleStartingWith(*s1, *s2, scc)
+			cycle = &c
+		}
+
+		if cycle != nil {
+			explainerWrapper := CycleExplainerWrapper{}
+			cycleCase := explainerWrapper.ExplainCycle(explainer, *cycle)
+			if v.FilterEx != nil && !v.FilterEx(&cycleCase) {
+				continue
+			}
+			cases = append(cases, cycleCase)
+		}
+	}
+	return cases
 }
 
-// CycleCasesInScc searches a single SCC for cycle anomalies
-func CycleCasesInScc(graph core.DirectedGraph, explainer core.DataExplainer, scc core.SCC) []CycleCase {
-	panic("impl me")
-}
-
-func init() {
-	// CycleAnomalySpecs =
+// CyclesWithDraw means "cycles!" in clojure.
+// TODO: This function contains some logic like draw, so I leave it unimplemented.
+func CyclesWithDraw() {
+	panic("implement me")
 }
