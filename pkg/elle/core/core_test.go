21f0639bfe3077b7fa116f579cc35ad665161d74
you06 <you1474600@gmail.com>
2020-04-22 23:22:13 +0800

    Implement elle core (#192)
    Signed-off-by: you06 <you1474600@gmail.com>

---
+++ pkg/elle/core/core_test.go
@@ -0,0 +1,35 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProcessGraph(t *testing.T) {
+	history, err := ParseHistory(`{:index 0 :process 1 :type :ok}
+{:index 1 :process 2 :type :ok}
+{:index 2 :process 2 :type :ok}
+{:index 3 :process 1 :type :ok}`)
+
+	var (
+		v0 = Vertex{Value: history[0]}
+		v1 = Vertex{Value: history[1]}
+		v2 = Vertex{Value: history[2]}
+		v3 = Vertex{Value: history[3]}
+	)
+	processGraph := DirectedGraph{
+		Outs: map[Vertex]map[Vertex][]Rel{
+			v0: {
+				v3: {Process},
+			},
+			v1: {
+				v2: {Process},
+			},
+		},
+	}
+
+	_, g, _ := ProcessGraph(history)
+	assert.Equal(t, err, nil, "test process graph, parse history")
+	assert.Equal(t, g, processGraph)
+}
