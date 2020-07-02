abcb5fb379204a7cf7f8ba2cab31ea08091a242f
mahjonp <junpeng.man@gmail.com>
2020-07-02 12:41:43 +0800

    fix unknown flag on old release version (#289)
    Signed-off-by: mahjonp <junpeng.man@gmail.com>

--- pkg/test-infra/tidb/template.go
+++ pkg/test-infra/tidb/template.go
@@ -230,7 +230,6 @@ POD_NAME=${POD_NAME:-$HOSTNAME}
 ARGS="--pd=http://${CLUSTER_NAME}-pd:2379 \
 --advertise-addr=${POD_NAME}.${HEADLESS_SERVICE_NAME}.${NAMESPACE}.svc:20160 \
 --addr=0.0.0.0:20160 \
---advertise-status-addr=${POD_NAME}.${HEADLESS_SERVICE_NAME}.${NAMESPACE}.svc:20180 \
 --status-addr=0.0.0.0:20180 \
 --data-dir={{.DataDir}} \
 --capacity=${CAPACITY} \
