3fe3b4cbe066887c300fda2cf2c1dacd93cd5331
mahjonp <junpeng.man@gmail.com>
2021-02-22 15:04:14 +0800

    address comment
    Signed-off-by: mahjonp <junpeng.man@gmail.com>

--- util/proxy.go
+++ util/proxy.go
@@ -3,10 +3,12 @@ package util
 import (
 	"context"
 	"net"
+	"net/url"
+	"os"
 	"sync"
 
 	"github.com/go-sql-driver/mysql"
-	"golang.org/x/net/proxy"
+	netproxy "golang.org/x/net/proxy"
 )
 
 var (
@@ -17,11 +19,63 @@ var (
 // and makes underlying connections directly.
 // ALL_PROXY/all_proxy, NO_PROXY/no_proxy
 func SetMySQLProxyFromEnvironment() {
-	dialer := proxy.FromEnvironment()
+	dialer := fromEnvironment()
 
 	setMySQLProxyOnce.Do(func() {
 		mysql.RegisterDialContext("tcp", func(ctx context.Context, addr string) (net.Conn, error) {
+			if xd, ok := dialer.(netproxy.ContextDialer); ok {
+				return xd.DialContext(ctx, "tcp", addr)
+			}
 			return dialer.Dial("tcp", addr)
 		})
 	})
+}
+
+var (
+	kubeProxyEnv = &envOnce{
+		names: []string{"KUBE_PROXY", "kube_proxy"},
+	}
+)
+
+// envOnce looks up an environment variable (optionally by multiple
+// names) once. It mitigates expensive lookups on some platforms
+// (e.g. Windows).
+// (Borrowed from net/proxy/proxy.go)
+type envOnce struct {
+	names []string
+	once  sync.Once
+	val   string
+}
+
+func (e *envOnce) Get() string {
+	e.once.Do(e.init)
+	return e.val
+}
+
+func (e *envOnce) init() {
+	for _, n := range e.names {
+		e.val = os.Getenv(n)
+		if e.val != "" {
+			return
+		}
+	}
+}
+
+func fromEnvironment() netproxy.Dialer {
+	direct := &net.Dialer{Resolver: &net.Resolver{PreferGo: true}}
+	kubeProxy := kubeProxyEnv.Get()
+
+	if len(kubeProxy) == 0 {
+		return direct
+	}
+	proxyURL, err := url.Parse(kubeProxy)
+
+	if err != nil {
+		return direct
+	}
+	proxy, err := netproxy.FromURL(proxyURL, direct)
+	if err != nil {
+		return direct
+	}
+	return proxy
 }
