d253828e74cded451a5c72c60c02d6dfb25f7f0b
vivid392845427 <82034163+vivid392845427@users.noreply.github.com>
2021-07-13 09:15:43 +0800

    bank2: ignore error 1105 without message (#426)
    * When the error code is 1105 and the error message is empty, ignore the failure
    
    * Update testcase/bank2/client.go
    
    Co-authored-by: zyguan <zhongyangguan@gmail.com>
    
    Co-authored-by: zyguan <zhongyangguan@gmail.com>

--- testcase/bank2/client.go
+++ testcase/bank2/client.go
@@ -391,7 +391,8 @@ func (c *bank2Client) verify(ctx context.Context, db *sql.DB) {
 				strings.Contains(errStr, "TiKV server timeout") ||
 				strings.Contains(errStr, "redirect failed") ||
 				strings.Contains(errStr, "no leader") ||
-				strings.Contains(errStr, "injected")) {
+				strings.Contains(errStr, "injected") ||
+				(errStr == "Error 1105: ")) {
 			atomic.StoreInt32(&c.stop, 1)
 			c.wg.Wait()
 			log.Fatalf("[%s] ADMIN CHECK TABLE bank2_accounts fails: %v", c, err)
